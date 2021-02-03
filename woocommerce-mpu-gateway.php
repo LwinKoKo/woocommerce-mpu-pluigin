3da5d367293272ede14db7d491f08949e8b767b8
LwinKoKo <38004357+LwinKoKo@users.noreply.github.com>
2021-02-03 09:54:27 +0630

    Update woocommerce-mpu-gateway.php

--- woocommerce-mpu-gateway.php
+++ woocommerce-mpu-gateway.php
@@ -27,79 +27,79 @@ function init_mpu_gateway_class() {
 	class AES extends WC_Payment_Gateway{
 		
 		protected $cipher;
-		protected $mode;
-		protected $pad_method;
-		protected $secret_key;
-		protected $iv;
-	 
-		public function __construct($key, $method = 'pkcs7', $iv = '', $mode = MCRYPT_MODE_ECB, $cipher = MCRYPT_RIJNDAEL_128)
-		{
-			$this->secret_key = $key;
-			$this->pad_method =$method;
-			$this->iv = $iv;
-			$this->mode = $mode;
-			$this->cipher = $cipher;
-		}
-	 
-		protected function pad_or_unpad($str, $ext)
-		{
-			if (!is_null($this->pad_method)) {
-				$func_name = __CLASS__ . '::' . $this->pad_method . '_' . $ext . 'pad';
-				if (is_callable($func_name)) {
-					$size = mcrypt_get_block_size($this->cipher, $this->mode);
-					return call_user_func($func_name, $str, $size);
-				}
-			}
-			return $str;
-		}
-	 
-		protected function pad($str)
-		{
-			return $this->pad_or_unpad($str, '');
-		}
-	 
-		protected function unpad($str)
-		{
-			return $this->pad_or_unpad($str, 'un');
-		}
-	 
-		public function encrypt($str)
-		{
-			$str = $this->pad($str);
-			$td = mcrypt_module_open($this->cipher, '', $this->mode, '');
-			if (empty($this->iv)) {
-				$iv = @mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-			} else {
-				$iv = $this->iv;
-			}
-			
-			mcrypt_generic_init($td, $this->secret_key, $iv);
-			$cyper_text = mcrypt_generic($td, $str);
-			$rt = base64_encode($cyper_text);
-			mcrypt_generic_deinit($td);
-			mcrypt_module_close($td);
-			return $rt;
-		}
+        protected $mode;
+        protected $pad_method;
+        protected $secret_key;
+        protected $iv;
+     
+        public function __construct($key, $method = 'pkcs7', $iv = '', $mode = MCRYPT_MODE_ECB, $cipher = MCRYPT_RIJNDAEL_128)
+        {
+            $this->secret_key = $key;
+            $this->pad_method =$method;
+            $this->iv = $iv;
+            $this->mode = $mode;
+            $this->cipher = $cipher;
+        }
+     
+        protected function pad_or_unpad($str, $ext)
+        {
+            if (!is_null($this->pad_method)) {
+                $func_name = __CLASS__ . '::' . $this->pad_method . '_' . $ext . 'pad';
+                if (is_callable($func_name)) {
+                    $size = mcrypt_get_block_size($this->cipher, $this->mode);
+                    return call_user_func($func_name, $str, $size);
+                }
+            }
+            return $str;
+        }
+     
+        protected function pad($str)
+        {
+            return $this->pad_or_unpad($str, '');
+        }
+     
+        protected function unpad($str)
+        {
+            return $this->pad_or_unpad($str, 'un');
+        }
+     
+        public function encrypt($str)
+        {
+            $str = $this->pad($str);
+            $td = mcrypt_module_open($this->cipher, '', $this->mode, '');
+            if (empty($this->iv)) {
+                $iv = @mcrypt_create_iv(mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
+            } else {
+                $iv = $this->iv;
+            }
+            
+            mcrypt_generic_init($td, $this->secret_key, $iv);
+            $cyper_text = mcrypt_generic($td, $str);
+            $rt = base64_encode($cyper_text);
+            mcrypt_generic_deinit($td);
+            mcrypt_module_close($td);
+            return $rt;
+        }
 
-		function decrypt($ciphertext) {
-			$chiperRaw = base64_decode($ciphertext);
-			$originalData = openssl_decrypt($chiperRaw, 'AES-256-ECB', $this->secret_key, OPENSSL_RAW_DATA);
-			return $originalData;
-		}
-	 
-		public static function pkcs7_pad($text, $blocksize)
-		{
-			$pad = $blocksize - (strlen($text) % $blocksize);
-			return $text . str_repeat(chr($pad), $pad);
-		}
-	 
-		public static function pkcs7_unpad($text)
-		{
-			$pad = ord($text[strlen($text) - 1]);
-			if ($pad > strlen($text)) return false;
-			if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) return false;
-			return substr($text, 0, -1 * $pad);
-		}
+    	function decrypt($ciphertext) {
+    		$chiperRaw = base64_decode($ciphertext);
+    		$originalData = openssl_decrypt($chiperRaw, 'AES-256-ECB', $this->secret_key, OPENSSL_RAW_DATA);
+    		return $originalData;
+    	}
+     
+        public static function pkcs7_pad($text, $blocksize)
+        {
+            $pad = $blocksize - (strlen($text) % $blocksize);
+            return $text . str_repeat(chr($pad), $pad);
+        }
+     
+        public static function pkcs7_unpad($text)
+        {
+            $pad = ord($text[strlen($text) - 1]);
+            if ($pad > strlen($text)) return false;
+            if (strspn($text, chr($pad), strlen($text) - $pad) != $pad) return false;
+            return substr($text, 0, -1 * $pad);
+        }
 	}
  
 	class WC_MPU_Gateway extends WC_Payment_Gateway {
@@ -150,10 +150,10 @@ function init_mpu_gateway_class() {
  
 			$this->form_fields = array(
 				'enabled' => array(
-				'title'   => __( 'Enable/Disable', 'wc-mpu-gateway' ),
-				'type'    => 'checkbox',
-				'label'   => __( 'Enable MPU Payment', 'wc-mpu-gateway' ),
-				'default' => 'yes'
+					'title'   => __( 'Enable/Disable', 'wc-mpu-gateway' ),
+					'type'    => 'checkbox',
+					'label'   => __( 'Enable MPU Payment', 'wc-mpu-gateway' ),
+					'default' => 'yes',
 				),
 				'title' => array(
 					'title'       => __( 'Title', 'wc-mpu-gateway' ),
@@ -206,7 +206,7 @@ function init_mpu_gateway_class() {
 			if ( $this->description ) {
 				// you can instructions for test mode, I mean test card numbers etc.
 				if ( $this->testmode ) {
-					$this->description  = ' TEST MODE ENABLED. In test mode, you can use the card numbers listed in <a href="#" target="_blank" rel="noopener noreferrer">documentation</a>.';
+					$this->description  = ' TEST MODE ENABLED. In test mode, please do not use your real personal information.</a>.';
 					$this->description  = trim( $this->description );
 				}
 				// display the description with <p> tags etc.
@@ -229,7 +229,7 @@ function init_mpu_gateway_class() {
 		}
  
 		/*
-		 * We're processing the payments here, everything about it is in Step 5
+		 * Processing the payments and redirect to the receipt page
 		 */
 		public function process_payment( $order_id ) {
  
@@ -259,7 +259,7 @@ function init_mpu_gateway_class() {
 			global $woocommerce;
             $order = new WC_Order( $order_id );
 			
-			//$aes = new AES('n6atpjz75hw213yfldg80ocreb9vukiq');
+			$aes = new AES('n6atpjz75hw213yfldg80ocreb9vukiq'); //key //‘transactionid|amount|companyId|paymenttype’
 			
             $redirect_url=$this->get_return_url( $order );
 			
@@ -269,8 +269,7 @@ function init_mpu_gateway_class() {
 			$amount = $order->order_total;
 			$companyId = $this->company_id;
 			$transactionid = '0MPUMTG00'.$order_id;
-			//$encryptedString = $aes->encrypt($transactionid.'|'.$amount.'|'.$companyId.'|'.$service_provider);
-			$encryptedString = '';
+			$encryptedString = $aes->encrypt($transactionid.'|'.$amount.'|'.$companyId.'|'.$service_provider);
 			$phonenumber = $this->phone_no;
 			
 			if($this->testmode){
@@ -282,7 +281,6 @@ function init_mpu_gateway_class() {
 				$url  = 'https://www.mpu-ecommerce.com/Payment/Payment/pay?';
 			}
 			
-			/*
 			$body = array(
 				'paymenttype' => $service_provider,
 				'encryptedString' => $encryptedString,
@@ -293,8 +291,8 @@ function init_mpu_gateway_class() {
 				'serviceData' => 'Payment',
 				'sandbox' => $sandbox,
 			);
-			 
-			$result = json_decode(httpPost($url, $test_param, 'json'));
+			
+			$result = json_decode($this->httpPost('http://paymentuat.mmpaymal.com/payment/paymentapi', $body, 'json'));
 			$decrypted_value = $aes->decrypt($result->encryptedString);
 			$mpu = json_decode($decrypted_value);
 		
@@ -309,20 +307,6 @@ function init_mpu_gateway_class() {
 				'amount' => $mpu->amount,
 				'hashValue' => $mpu->hashValue,
 			);
-			*/
-			
-			$mpu_url = array(
-				'currencyCode' => '104',
-				'merchantID' => '203104001003144',
-				'invoiceNo' => '0MPUMTF65',
-				'productDesc' => 'Thirdparty',
-				'userDefined1' => 'userdefined1',
-				'userDefined2' => 'userdefined2',
-				'userDefined3' => 'mtg',
-				'amount' => '000000100000',
-				'hashValue' => 'A3B5B4F0605584637E8CC4D3AE77D9CF79CC1E86',
-			);
-			
             
             $strHtml = '';
 			$strHtml .= '<form action="' . esc_url($url) . '" method="post" id="form">';
@@ -339,15 +323,15 @@ function init_mpu_gateway_class() {
         }
 		
 		function httpPost($url, $data){
-			$curl = curl_init($url);
-			curl_setopt($curl, CURLOPT_POST, true);
-			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
-			curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			$response = curl_exec($curl);
-			curl_close($curl);
-			return $response;
-		}
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_POST, true);
+        	curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+        	curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($curl);
+            curl_close($curl);
+            return $response;
+        }
  
 		/*
 		 * In case you need a webhook, like PayPal IPN etc
